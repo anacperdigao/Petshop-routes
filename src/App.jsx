f5d7e954d2c1bffcd71a3a64cd196903ff52341e
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-07 16:18:32 -0300

    Finalizando a chamada de API

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
 function App() {
-// Terceiro passo: Usar o Router pra mostrar que eu terei alternancia de paginas e de rotas
-// Quarto passo: Usar Route que eu importei tbm para aninhar cada componente que será uma rota
-// Quinto passo: Dar o path de cada um no dominio web (usei exact pra prevenir um comportamento padrao do React 
+// Passo 3: Usar o Router pra mostrar que eu terei alternancia de paginas e de rotas
+// Passo 4: Usar Route que eu importei tbm para aninhar cada componente que será uma rota
+// Passo 5: Dar o path de cada um no dominio web (usei exact pra prevenir um comportamento padrao do React 
 // que entende que '/' está dentro de '/sobre')
-// Sexto passo: O switch serve pra fazer o React parar de procurar outras rotas se ele ja achou a rota certa
-// Setimo passo: Agora preciso indicar caso a pessoa tente acessar uma rota que nao existe, entao vou criar uma nova
+// Passo 6: O switch serve pra fazer o React parar de procurar outras rotas se ele ja achou a rota certa
+// Passo 7: Agora preciso indicar caso a pessoa tente acessar uma rota que nao existe, entao vou criar uma nova
 // página chamada "Pagina 404.jsx". IR PARA LA. Na <Pagina404> eu nao vou indicar caminho pq eu vou dizer que
 // qualquer caminho sem ser os que existem, vão direcionar pra la.
-// Oitavo passo foi criar o componente Cabecalho que la tinha o caminho das rotas para usar. Agora vou importar aqui.
-// Nono passo: Inserir o componente antes do Switch, assim eu garanto que ele vai englobar todas as rotas.
-// Decimo passo: Esta acontencendo um problema que o react ta chamando o index.html toda vez que eu mudo a rota,
+// Passo 8 foi criar o componente Cabecalho que la tinha o caminho das rotas para usar. Agora vou importar aqui.
+// Passo 9: Inserir o componente antes do Switch, assim eu garanto que ele vai englobar todas as rotas.
+// Passo 10: Esta acontencendo um problema que o react ta chamando o index.html toda vez que eu mudo a rota,
 // e para isso eu vou voltar no componente Cabecalho e vou trocar o <a> por <link>, e aí sim ele nao vai chamar 
 // pelo index.html toda vez. Ou seja, o <Link> faz o papel do ancora <a> só que sem recarregar a pagina toda hora.
+// Passo 11: Agora vou consumir uma API do arquivo local chamado db.json e pra isso preciso instalar no node.js
+// o seguinte comando "npx json-server --watch db.json --port 5000". Isso é para alimentar os posts do blog.
+// Passo 12: Criar um arquivo chamado api.js 
+// Passo 13: Verificar se ja tem a biblioteca axios instalada pq é ela que faz a conexao com a API.
+// Passo 14: Criar um componente de ListaPost para pegar os dados da API.
+
 
   return (
     <Router>
